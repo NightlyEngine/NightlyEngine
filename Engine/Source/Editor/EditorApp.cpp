b73f44088b5e64af4eb1d72095e142697320a6d3
Futureblur <itsfutureblur@gmail.com>
2022-04-17 20:18:24 +0200

    Update EditorApp.cpp so it launches a new application

--- Engine/Source/Editor/EditorApp.cpp
+++ Engine/Source/Editor/EditorApp.cpp
@@ -1,6 +1,40 @@
-#include <iostream>
+#include "Runtime/Core/Application.h"
+
+namespace NightlyEditor
+{
+	class EditorApp : public Nightly::Application
+	{
+	public:
+		EditorApp() = default;
+
+		~EditorApp() override = default;
+
+		void Launch() override
+		{
+			OnStart();
+		}
+
+	private:
+		void OnStart() override
+		{
+			std::cout << "Hello from Nightly Editor!\n";
+		}
+
+		void OnUpdate() override
+		{
+
+		}
+
+		void OnQuit() override
+		{
+
+		}
+	};
+}
 
 int main(int argc, char** argv)
 {
-	std::cout << "Hello from Nightly Editor!\n";
+	auto app = new NightlyEditor::EditorApp;
+	app->Launch();
+	delete app;
 }
