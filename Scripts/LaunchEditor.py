f8ee521eb641c90cbf786ba2257a4891c97c5b9a
Futureblur <itsfutureblur@gmail.com>
2022-04-19 13:54:19 +0200

    Modify LaunchEditor.py so it works with the new binaries directory

--- Scripts/LaunchEditor.py
+++ Scripts/LaunchEditor.py
@@ -1,76 +1,82 @@
 from os.path import exists
 import os
-import sys
-import shutil
+import platform
+
 
 # Set console colors
 class colors:
     OKCYAN = '\033[96m'
-    OKGREEN = '\033[92m'
-    WARNING = '\033[93m'
     FAIL = '\033[91m'
     ENDC = '\033[0m'
     BOLD = '\033[1m'
-    
+
 
 # Build configuration
 config = "Debug"
 
 # Set paths
-runtime_path = f"Engine/Build/{config}/Engine/Source/Runtime"
-editor_path = f"Engine/Build/{config}/Engine/Source/Editor"
+binary_path = f"Engine/Binaries/{config}"
 
+engine_runtime_name = ""
 dylib_name = "libNightlyEngine.dylib"
-editor_name = "NightlyEditor"
+dll_name = "NightlyEngine.dll"
+so_name = "NightlyEngine.so"
+
+editor_name = ""
+editor_app_name = "NightlyEditor"
+editor_exe_name = "NightlyEditor.exe"
 
 # Go up one directory
 os.chdir("..")
 
+print(colors.BOLD + colors.OKCYAN + "==> Checking operating system..." + colors.ENDC)
+
+# Check operating system
+os_name = platform.system()
+if os_name == "Windows":
+    engine_runtime_name = dll_name
+    editor_name = editor_exe_name
+elif os_name == "Darwin":
+    engine_runtime_name = dylib_name
+    editor_name = editor_app_name
+elif os_name == "Linux":
+    engine_runtime_name = so_name
+    editor_name = editor_app_name
+else:
+    print(colors.FAIL + "Could not detect operating system!" + colors.ENDC)
+    exit()
+
+print("Detected OS: " + os_name)
+
+print(colors.BOLD + colors.OKCYAN + "==> Check complete." + colors.ENDC)
+
 print(colors.BOLD + colors.OKCYAN + "==> Validating..." + colors.ENDC)
 
-# Some generators like Xcode add the output files inside a <CONFIGURATION> folder.
-# This prefix will be set to that <CONFIGURATION> folder if such a generator is being used.
-prefix="/"
-
-engine_path_0 = runtime_path + "/" + dylib_name
-engine_path_1 = runtime_path + "/" + config + "/" + dylib_name
+engine_path = binary_path + "/" + engine_runtime_name
 
 # Check if engine library exists
-if not exists(engine_path_0):
-    if exists(engine_path_1):
-        prefix=f"/{config}/"
-    else:
-        print(colors.FAIL + "Engine runtime library was not found in search paths. Expected it to be in:")
-        print("\'" + engine_path_0 + "\'" + " or \'" + engine_path_1 + "\'" + colors.ENDC)
-        exit()
-    
+if not exists(engine_path):
+    print(colors.FAIL + "Engine runtime library was not found in search paths. Expected it to be in:")
+    print(f"'{engine_path}'" + colors.ENDC)
+    exit()
+
 print("Found engine runtime library.")
-    
-editor_search_path = editor_path + prefix + editor_name
+
+editor_path = binary_path + "/" + editor_name
 
 # Check if editor application exists
-if not exists(editor_search_path):
+if not exists(editor_path):
     print(colors.FAIL + "Editor was not found in search paths. Expected it to be in:")
-    print("\'" + editor_search_path + "\'" + colors.ENDC)
+    print(f"'{editor_path}'" + colors.ENDC)
     exit()
-    
+
 print("Found editor application.")
 
 print(colors.BOLD + colors.OKCYAN + "==> Validation complete, no errors found." + colors.ENDC)
-print("Copying engine library into editor folder...")
-
-# Copy library into editor folder
-try:
-    shutil.copyfile(runtime_path + prefix + dylib_name, editor_path + prefix + dylib_name)
-except:
-    print("Failed to copy!")
-    exit()
-    
-print("Copy successful.")
 print(colors.BOLD + colors.OKCYAN + "==> Launching Nightly Editor..." + colors.ENDC)
 
 # Wish the user a good time
 print("Have fun and remember to stay hydrated!")
 
 # Launch Nightly
-os.system("./" + editor_search_path)
+os.system("./" + editor_path)
