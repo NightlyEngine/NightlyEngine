d4a62aae17a275d7117b66ebd119d6346eb7e2d2
Futureblur <itsfutureblur@gmail.com>
2022-04-20 22:35:38 +0200

    Remove __declspec(dllimport) since it was causing linker errors

--- Engine/Source/Runtime/Core/Core.h
+++ Engine/Source/Runtime/Core/Core.h
@@ -1,19 +1,19 @@
 #pragma once
 
-#ifdef NL_EXPORT    // NL_EXPORT
+#ifdef NL_EXPORT_DLL	// NL_EXPORT_DLL
 
-#ifdef _WIN32       // _WIN32
+#ifdef _WIN32       	// _WIN32
 #define NL_API __declspec(dllexport)
 #elif __APPLE__ || __linux__
 #define NL_API __attribute__((visibility("default")))
-#endif              // _WIN32
+#endif              	// _WIN32
 
-#else               // NL_EXPORT NOT DEFINED
+#else               	// NL_EXPORT_DLL NOT DEFINED
 
-#ifdef _WIN32       // _WIN32
-#define NL_API __declspec(dllimport)
+#ifdef _WIN32       	// _WIN32
+#define NL_API 
 #elif __APPLE__ || __linux__
 #define NL_API
 #endif              // _WIN32
 
-#endif              // NL_EXPORT
+#endif              	// NL_EXPORT_DLL
