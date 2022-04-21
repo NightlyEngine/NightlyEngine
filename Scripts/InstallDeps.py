5bc296dc6e40c983de146b51e1ef9a9c7f8ed21b
Futureblur <itsfutureblur@gmail.com>
2022-04-21 12:49:06 +0200

    Create InstallDeps.py that installs required packages for the current OS

---
+++ Scripts/InstallDeps.py
@@ -0,0 +1,28 @@
+from Utils import install_deps, get_os
+
+deps = []
+macos_deps = [ "cmake", "python" ]
+linux_deps = [ "clang", "cmake", "ninja-build", "libx11-dev", "libxrandr-dev", "libxinerama-dev", "libxcursor-dev", "libxi-dev" ]
+
+install_cmd = ""
+update_cmd = ""
+
+os_name = get_os()
+
+if os_name == "Linux":
+	deps = linux_deps
+	install_cmd = "sudo apt install"
+	update_cmd = "sudo apt update"
+
+elif os_name == "Darwin":
+	deps = macos_deps
+	install_cmd = "brew install"
+	update_cmd = "brew update"
+
+else:
+	print("No dependencies are needed for this OS!")
+	exit()
+
+
+install_deps(os_name, deps, install_cmd, update_cmd)
+print("Generate your project files next: python3 ./GenerateNinja.py")
