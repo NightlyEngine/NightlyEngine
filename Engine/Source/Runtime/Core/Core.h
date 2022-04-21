23b102b6fd6ab20180469b94fd7fc484bab55cd9
Futureblur <itsfutureblur@gmail.com>
2022-04-21 19:06:27 +0200

    Update formatting

--- Engine/Source/Runtime/Core/Core.h
+++ Engine/Source/Runtime/Core/Core.h
@@ -1,6 +1,6 @@
 #pragma once
 
-#ifdef NL_EXPORT_DLL	// NL_EXPORT_DLL
+#ifdef NL_EXPORT_DLL    // NL_EXPORT_DLL
 
 #ifdef _WIN32       	// _WIN32
 #define NL_API __declspec(dllexport)
@@ -10,10 +10,10 @@
 
 #else               	// NL_EXPORT_DLL NOT DEFINED
 
-#ifdef _WIN32       	// _WIN32
+#ifdef _WIN32           // _WIN32
 #define NL_API 
 #elif __APPLE__ || __linux__
 #define NL_API
-#endif              // _WIN32
+#endif                  // _WIN32
 
-#endif              	// NL_EXPORT_DLL
+#endif                  // NL_EXPORT_DLL
