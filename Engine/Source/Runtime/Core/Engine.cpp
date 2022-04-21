5aa3bfddb2528618774ddcd9f0a710cea2d7f7fd
Futureblur <itsfutureblur@gmail.com>
2022-04-21 22:20:23 +0200

    Create Engine.cpp

---
+++ Engine/Source/Runtime/Core/Engine.cpp
@@ -0,0 +1,22 @@
+#include "Engine.h"
+
+#include "Log.h"
+
+namespace Nightly
+{
+	void Engine::Start()
+	{
+		if (m_IsRunning)
+		{
+			NL_CORE_ERROR("Nightly Engine is already running. Multiple instances of the application are not allowed!", LogSource::ENGINE);
+			return;
+		}
+
+		m_IsRunning = true;
+
+		std::cout << "\n\n" << "Nightly Engine Copyright (C) 2022 Futureblur\n"
+		          << "Welcome to Nightly Engine! Enjoy your time and happy coding :)\n\n";
+
+		NL_CORE_INFO("Initializing Nightly Engine...", LogSource::ENGINE);
+	}
+}
