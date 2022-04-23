2a9853715d08da163e542e564a177db4ea8efe2d
Futureblur <itsfutureblur@gmail.com>
2022-04-23 18:19:24 +0200

    Update EditorApp.cpp to create an editor window on startup
    EditorApp will also update the window using the WindowManager class.
    When Quit() is called, the editor will destroy the window and
    terminate the GLFW library.

--- Engine/Source/Editor/EditorApp.cpp
+++ Engine/Source/Editor/EditorApp.cpp
@@ -1,6 +1,7 @@
 #include "Runtime/Core/Application.h"
 #include "Runtime/Core/Engine.h"
 #include "Core/Log.h"
+#include "Core/WindowManager.h"
 
 using namespace Nightly;
 
@@ -25,12 +26,26 @@ namespace NightlyEditor
 
 			m_IsRunning = true;
 
+			// Create engine app
 			m_EngineInstance = std::make_unique<Engine>();
 			m_EngineInstance->Start();
 
 			NL_CORE_INFO("Initializing Nightly Editor...", LogSource::EDITOR);
 
-			Update();
+			// Create editor window
+			WindowProps props;
+			props.title = "Nightly Editor";
+			props.width = 1280;
+			props.height = 720;
+
+			m_EditorWindow = WindowManager::Create(props);
+
+			// Update the editor as long as the window is not closed
+			while (!m_EditorWindow->ShouldClose())
+			{
+				Update();
+			}
+
 			Quit();
 		}
 
@@ -40,18 +55,27 @@ namespace NightlyEditor
 			if (!m_EngineInstance) return;
 
 			m_EngineInstance->Update();
+
+			m_EditorWindow->SwapBuffers();
+			WindowManager::PollEvents();
 		}
 
 		void Quit() override
 		{
+			NL_CORE_INFO("Quitting Nightly Editor...", LogSource::EDITOR);
+
 			if (m_EngineInstance)
 			{
 				m_EngineInstance->Quit();
 			}
+
+			m_EditorWindow->Destroy();
+			WindowManager::Terminate();
 		}
 
 	private:
 		std::unique_ptr<Engine> m_EngineInstance;
+		std::unique_ptr<Window> m_EditorWindow;
 	};
 }
 
