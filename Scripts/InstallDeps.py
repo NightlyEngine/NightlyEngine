30d8ae7876c0641406549577261ddf6c583fc3ea
Futureblur <itsfutureblur@gmail.com>
2022-04-23 20:16:45 +0200

    Add Linux dependencies for OpenGL

--- Scripts/InstallDeps.py
+++ Scripts/InstallDeps.py
@@ -1,8 +1,9 @@
 from Utils import install_deps, get_os
 
 deps = []
-macos_deps = [ "cmake", "python" ]
-linux_deps = [ "clang", "cmake", "ninja-build", "libx11-dev", "libxrandr-dev", "libxinerama-dev", "libxcursor-dev", "libxi-dev" ]
+macos_deps = ["cmake", "python"]
+linux_deps = ["clang", "cmake", "ninja-build", "libx11-dev", "libxrandr-dev", "libxinerama-dev", "libxcursor-dev",
+              "libxi-dev", "libglfw3-dev", "libgl1-mesa-dev", "libglu1-mesa-dev"]
 
 install_cmd = ""
 update_cmd = ""
@@ -10,19 +11,18 @@ update_cmd = ""
 os_name = get_os()
 
 if os_name == "Linux":
-	deps = linux_deps
-	install_cmd = "sudo apt install"
-	update_cmd = "sudo apt update"
+    deps = linux_deps
+    install_cmd = "sudo apt install"
+    update_cmd = "sudo apt update"
 
 elif os_name == "Darwin":
-	deps = macos_deps
-	install_cmd = "brew install"
-	update_cmd = "brew update"
+    deps = macos_deps
+    install_cmd = "brew install"
+    update_cmd = "brew update"
 
 else:
-	print("No dependencies are needed for this OS!")
-	exit()
-
+    print("No dependencies are needed for this OS!")
+    exit()
 
 install_deps(os_name, deps, install_cmd, update_cmd)
 print("Generate your project files next: python3 ./GenerateNinja.py")
