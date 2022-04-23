f4199fabb99d0a186ff123e0ed56cd1c9d304ee3
Futureblur <itsfutureblur@gmail.com>
2022-04-23 18:51:09 +0200

    Include cassert in debug mode

--- Engine/Source/Runtime/Core/Core.h
+++ Engine/Source/Runtime/Core/Core.h
@@ -7,5 +7,8 @@
 
 #define NL_ASSERT(condition, message, source) NL_CORE_FATAL(message, LogSource::source); return
 #else
+
+#include <cassert>
+
 #define NL_ASSERT(condition, message, source) assert(condition && message)
 #endif
