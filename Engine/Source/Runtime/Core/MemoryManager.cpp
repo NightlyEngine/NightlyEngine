eaf2c985cdbe25e6689ce9c8dc6ffa79a31ee48e
Futureblur <itsfutureblur@gmail.com>
2022-04-24 19:55:53 +0200

    Create MemoryManager class that keeps track of the allocated memory

---
+++ Engine/Source/Runtime/Core/MemoryManager.cpp
@@ -0,0 +1,39 @@
+#include "MemoryManager.h"
+
+#include "Log.h"
+
+#define NL_MEGABYTES 1000000
+#define NL_KILOBYTES 1000
+
+namespace Nightly
+{
+	void MemoryManager::LogUsage()
+	{
+		auto current = (float) m_CurrentAllocation;
+		const char* type = "BYTES";
+
+		// Convert to bigger types to avoid long numbers
+		if (m_CurrentAllocation >= NL_MEGABYTES)
+		{
+			current = (float) m_CurrentAllocation / NL_MEGABYTES;
+			type = "MB";
+		}
+		else if (m_CurrentAllocation >= NL_KILOBYTES)
+		{
+			current = (float) m_CurrentAllocation / NL_KILOBYTES;
+			type = "KB";
+		}
+
+		NL_CORE_INFO("Current memory usage: " << current << " " << type, ENGINE);
+	}
+
+	void MemoryManager::LogAllocation(std::string_view className, size_t size)
+	{
+		NL_CORE_INFO(className << " Class: Allocated " << size << " bytes", ENGINE);
+	}
+
+	void MemoryManager::LogDeallocation(std::string_view className, size_t size)
+	{
+		NL_CORE_INFO(className << ": Freed " << size << " bytes", ENGINE);
+	}
+}
---
+++ Engine/Source/Runtime/Core/MemoryManager.h
@@ -0,0 +1,44 @@
+#pragma once
+
+#include "EngineAPI.h"
+
+namespace Nightly
+{
+	// Keeps track of the total heap allocated memory.
+	class NL_API MemoryManager
+	{
+	public:
+		MemoryManager() = default;
+		~MemoryManager() = default;
+
+		// Increases the current allocation by the size
+		// and logs the current state to the console.
+		static void Allocate(std::string_view className, size_t size)
+		{
+			LogAllocation(className, size);
+			m_CurrentAllocation += size;
+			LogUsage();
+		}
+
+		// Decreases the current allocation by the size
+		// and logs the current state to the console.
+		static void Free(std::string_view className, size_t size)
+		{
+			LogDeallocation(className, size);
+			m_CurrentAllocation -= size;
+			LogUsage();
+		}
+
+	private:
+		// Prints the current heap memory usage to the console.
+		static void LogUsage();
+
+		// Prints how much memory the class allocated in the heap.
+		static void LogAllocation(std::string_view className, size_t size);
+
+		// Prints how much memory was freed by the class in the heap.
+		static void LogDeallocation(std::string_view className, size_t size);
+
+		static inline size_t m_CurrentAllocation;
+	};
+}
