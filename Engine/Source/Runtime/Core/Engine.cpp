3bd265c95fdd125c7824506786e9da5f6bde1a7a
Futureblur <itsfutureblur@gmail.com>
2022-04-24 12:38:31 +0200

    Assert if multiple instances of the engine app were detected

--- Engine/Source/Runtime/Core/Engine.cpp
+++ Engine/Source/Runtime/Core/Engine.cpp
@@ -1,27 +1,24 @@
 #include "Engine.h"
 
+#include "Core.h"
 #include "Log.h"
 
 namespace Nightly
 {
 	void Engine::Start()
 	{
-		if (m_IsRunning)
-		{
-			NL_CORE_ERROR("Nightly Engine is already running. Multiple instances of the application are not allowed!", LogSource::ENGINE);
-			return;
-		}
+		NL_ASSERT(!m_IsRunning, "Nightly Editor is already running. Multiple instances of the application are not allowed!", ENGINE);
 
 		m_IsRunning = true;
 
 		std::cout << "\n\n" << "Nightly Engine Copyright (C) 2022 Futureblur\n"
 		          << "Welcome to Nightly Engine! Enjoy your time and happy coding :)\n\n";
 
-		NL_CORE_INFO("Initializing Nightly Engine...", LogSource::ENGINE);
+		NL_CORE_INFO("Initializing Nightly Engine...", ENGINE);
 	}
 
 	void Engine::Quit()
 	{
-		NL_CORE_INFO("Quitting Nightly Engine...", LogSource::ENGINE);
+		NL_CORE_INFO("Quitting Nightly Engine...", ENGINE);
 	}
 }
