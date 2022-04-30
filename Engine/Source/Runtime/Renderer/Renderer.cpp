1ecd831bc0bdbd639401f551e373c378f050463d
Futureblur <itsfutureblur@gmail.com>
2022-04-30 18:14:38 +0200

    Load necessary shaders when the renderer gets initialized

--- Engine/Source/Runtime/Renderer/Renderer.cpp
+++ Engine/Source/Runtime/Renderer/Renderer.cpp
@@ -5,6 +5,7 @@
 
 #include "Core/Core.h"
 #include "Core/Log.h"
+#include "Shader.h"
 
 namespace Nightly
 {
@@ -12,5 +13,15 @@ namespace Nightly
 	{
 		NL_CORE_INFO("Initializing Renderer...", ENGINE);
 		NL_ASSERT(gladLoadGLLoader((GLADloadproc) glfwGetProcAddress), "Failed to initialize OpenGL library!", ENGINE);
+
+		// FIXME: Shader file paths are invalid when engine gets shipped
+		m_VertexShader = std::make_unique<Shader>(GL_VERTEX_SHADER, "../../Source/Shaders/UnlitVertexShader.glsl");
+		m_FragmentShader = std::make_unique<Shader>(GL_FRAGMENT_SHADER, "../../Source/Shaders/UnlitFragmentShader.glsl");
+
+		m_ShaderProgram = std::make_unique<ShaderProgram>();
+		m_ShaderProgram->Attach(m_VertexShader.get());
+		m_ShaderProgram->Attach(m_FragmentShader.get());
+		m_ShaderProgram->Link();
+		m_ShaderProgram->Use();
 	}
 }
--- Engine/Source/Runtime/Renderer/Renderer.h
+++ Engine/Source/Runtime/Renderer/Renderer.h
@@ -1,13 +1,18 @@
 #pragma once
 
 #include "Core/EngineAPI.h"
+#include "Shader.h"
 
 namespace Nightly
 {
 	class NL_API Renderer
 	{
 	public:
-		// Initializes the OpenGL library.
+		// Initializes the OpenGL library and necessary loads shaders.
 		static void Initialize();
+
+	private:
+		static inline std::unique_ptr<Shader> m_VertexShader, m_FragmentShader;
+		static inline std::unique_ptr<ShaderProgram> m_ShaderProgram;
 	};
 }
