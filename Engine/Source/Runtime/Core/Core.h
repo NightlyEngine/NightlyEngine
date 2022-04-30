fa1fdd9611ab4592ce589907b5da85e730955160
Futureblur <itsfutureblur@gmail.com>
2022-04-30 17:52:44 +0200

    Create `NL_TEXT` macro for concatenating multiple data types

--- Engine/Source/Runtime/Core/Core.h
+++ Engine/Source/Runtime/Core/Core.h
@@ -12,3 +12,6 @@
 
 #define NL_ASSERT(condition, message, source) assert(condition && message)
 #endif
+
+// Creates a string stream for concatenating multiple data types.
+#define NL_TEXT(string) std::stringstream("") << string
