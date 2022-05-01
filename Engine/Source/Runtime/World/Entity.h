a06789c61d208934d8b478ccc0e7e22309d639a6
Futureblur <itsfutureblur@gmail.com>
2022-05-01 16:00:57 +0200

    Create additional `Entity` constructor that takes in a `UUID`
    This constructor will be used later for serialization, so that the
    asset serializer can directly pass in the stored UUID.

--- Engine/Source/Runtime/World/Entity.h
+++ Engine/Source/Runtime/World/Entity.h
@@ -2,16 +2,22 @@
 
 #include "Core/EngineAPI.h"
 #include "Core/ITrackable.h"
+#include "Core/UUID.h"
 
 namespace Nightly
 {
 	// An entity is an object that can be spawned in a world
 	// that has components which define how it should behave.
-	class NL_API Entity : public ITrackable<Entity>
+	class NL_API Entity : public ITrackable<Entity>, public UUID
 	{
 	public:
 		explicit Entity(std::string_view name = "New Entity", std::string_view tag = "Default")
 				: m_Name(name), m_Tag(tag)
+		{
+		}
+
+		Entity(std::string_view name, std::string_view tag, uint64_t uuid)
+				: m_Name(name), m_Tag(tag), UUID(uuid)
 		{
 		}
 
