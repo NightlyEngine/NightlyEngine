a4e21fa19cebe281a2807a9f0a08151fdc13c60c
Futureblur <itsfutureblur@gmail.com>
2022-05-01 11:57:42 +0200

    Create functions for finding entities

--- Engine/Source/Runtime/World/World.cpp
+++ Engine/Source/Runtime/World/World.cpp
@@ -7,10 +7,68 @@ namespace Nightly
 {
 	World::~World() = default;
 
-	void World::AddEntity(std::shared_ptr<Entity>& entity)
+	void World::AddEntity(const std::shared_ptr<Entity>& entity)
 	{
 		m_EntityRegistry.push_back(entity);
 		NL_CORE_INFO("Registered entity: " << entity->GetName(), ENGINE);
+	}
+
+	std::shared_ptr<Entity> World::FindEntity(std::string_view name)
+	{
+		for (const auto& entity : m_EntityRegistry)
+		{
+			if (entity->GetName() == name)
+			{
+				return entity;
+			}
+		}
+
+		return nullptr;
+	}
+
+	std::shared_ptr<Entity> World::FindEntityByTag(std::string_view tag)
+	{
+		for (const auto& entity : m_EntityRegistry)
+		{
+			if (entity->GetTag() == tag)
+			{
+				return entity;
+			}
+		}
+
+		return nullptr;
+	}
+
+	bool World::FindEntities(std::string_view name, EntityList& outList)
+	{
+		bool found = false;
+
+		for (const auto& entity : m_EntityRegistry)
+		{
+			if (entity->GetName() == name)
+			{
+				found = true;
+				outList.push_back(entity);
+			}
+		}
+
+		return found;
+	}
+
+	bool World::FindEntitiesByTag(std::string_view tag, EntityList& outList)
+	{
+		bool found = false;
+
+		for (const auto& entity : m_EntityRegistry)
+		{
+			if (entity->GetTag() == tag)
+			{
+				found = true;
+				outList.push_back(entity);
+			}
+		}
+
+		return found;
 	}
 
 	void World::RemoveEntity(std::shared_ptr<Entity>& entity)
--- Engine/Source/Runtime/World/World.h
+++ Engine/Source/Runtime/World/World.h
@@ -7,6 +7,8 @@ namespace Nightly
 {
 	class Entity;
 
+	using EntityList = std::vector<std::shared_ptr<Entity>>;
+
 	class NL_API World : public ITrackable<World>
 	{
 	public:
@@ -20,7 +22,29 @@ namespace Nightly
 		NL_DEFINE_TRACKABLE(World);
 
 		// Adds entity to the world registry.
-		void AddEntity(std::shared_ptr<Entity>& entity);
+		void AddEntity(const std::shared_ptr<Entity>& entity);
+
+		// Scans the entire entity registry and returns
+		// the first entity that matches the specified name.
+		// Returns nullptr if it was not found.
+		std::shared_ptr<Entity> FindEntity(std::string_view name);
+
+		// Scans the entire entity registry and returns
+		// the first entity that matches the specified tag.
+		// Returns nullptr if it was not found.
+		std::shared_ptr<Entity> FindEntityByTag(std::string_view tag);
+
+		// Scans the entire entity registry and populates outList
+		// with all entities that match the specified name.
+		// If nothing was found, outList remains untouched.
+		// Returns whether at least one entity was found.
+		bool FindEntities(std::string_view name, EntityList& outList);
+
+		// Scans the entire entity registry and populates outList
+		// with all entities that match the specified tag.
+		// If nothing was found, outList remains untouched.
+		// Returns whether at least one entity was found.
+		bool FindEntitiesByTag(std::string_view tag, EntityList& outList);
 
 		// Removes entity from the world registry.
 		void RemoveEntity(std::shared_ptr<Entity>& entity);
