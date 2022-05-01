8025cb21d8c5e062b06427b550b3c23f90c6e076
Futureblur <itsfutureblur@gmail.com>
2022-05-01 15:56:41 +0200

    Create `UUID` class for generating random ids

---
+++ Engine/Source/Runtime/Core/UUID.cpp
@@ -0,0 +1,14 @@
+#include "UUID.h"
+
+#include <random>
+
+namespace Nightly
+{
+	static std::random_device m_Device;
+	static std::mt19937_64 m_Engine(m_Device());
+	static std::uniform_int_distribution<uint64_t> m_Distribution;
+
+	UUID::UUID() : m_UUID(m_Distribution(m_Engine))
+	{
+	}
+}
---
+++ Engine/Source/Runtime/Core/UUID.h
@@ -0,0 +1,43 @@
+#pragma once
+
+#include "EngineAPI.h"
+
+namespace Nightly
+{
+	class UUID
+	{
+	public:
+		UUID();
+
+		explicit UUID(uint64_t uuid) : m_UUID(uuid)
+		{
+		}
+
+		virtual ~UUID() = default;
+
+		explicit operator uint64_t() const
+		{
+			return m_UUID;
+		}
+
+		[[nodiscard]] const uint64_t& GetUUID() const
+		{
+			return m_UUID;
+		}
+
+	private:
+		uint64_t m_UUID;
+	};
+}
+
+namespace std
+{
+	template <>
+	struct hash<Nightly::UUID>
+	{
+		std::size_t operator()(const Nightly::UUID& uuid) const
+		{
+			return hash<uint64_t>()((uint64_t) uuid);
+		}
+	};
+}
