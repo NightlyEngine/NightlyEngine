1836ccc2fce798f5dc476fa5f5c143bfe41eaaa3
Futureblur <itsfutureblur@gmail.com>
2022-05-14 21:35:59 +0200

    Add component support to `Entity`

--- Engine/Source/Runtime/World/Entity.cpp
+++ Engine/Source/Runtime/World/Entity.cpp
@@ -1,8 +1,25 @@
 #include "Entity.h"
 
 #include "World.h"
+#include "Components/TransformComponent.h"
 
 namespace Nightly
 {
 	Entity::~Entity() = default;
+
+	void Entity::Initialize(const std::shared_ptr<World>& attachedWorld)
+	{
+		if (Transform()) return;
+
+		m_AttachedWorld = attachedWorld;
+
+		auto component = std::make_shared<TransformComponent>();
+		component->m_ParentEntity = GetPointer();
+		m_ComponentRegistry.push_back(component);
+	}
+
+	std::shared_ptr<Entity> Entity::GetPointer() const
+	{
+		return m_AttachedWorld->FindEntityById(GetUUID());
+	}
 }
--- Engine/Source/Runtime/World/Entity.h
+++ Engine/Source/Runtime/World/Entity.h
@@ -3,6 +3,8 @@
 #include "Core/EngineAPI.h"
 #include "Core/ITrackable.h"
 #include "Core/UUID.h"
+#include "Components/Component.h"
+#include "Components/TransformComponent.h"
 
 namespace Nightly
 {
@@ -14,7 +16,7 @@ namespace Nightly
 	{
 	public:
 		explicit Entity(std::string_view name = "New Entity", std::string_view tag = "Default")
-				: m_Name(name), m_Tag(tag)
+				: m_Name(name), m_Tag(tag), UUID()
 		{
 		}
 
@@ -26,6 +28,9 @@ namespace Nightly
 		~Entity() override;
 
 		NL_DEFINE_TRACKABLE(Entity);
+
+		// Creates transform component and sets the attached world.
+		void Initialize(const std::shared_ptr<World>& attachedWorld);
 
 		[[nodiscard]] const std::string& GetName() const
 		{
@@ -53,9 +58,79 @@ namespace Nightly
 			return m_AttachedWorld;
 		}
 
+		// Adds a component to the registry and returns
+		// whether the operation was successful.
+		template <typename T>
+		bool AddComponent(std::shared_ptr<T> component)
+		{
+			// Prevent adding another transform component
+			if (std::is_same_v<T, TransformComponent>)
+			{
+				return false;
+			}
+
+			component->m_ParentEntity = GetPointer();
+			m_ComponentRegistry.push_back(component);
+
+			return true;
+		}
+
+		// Removes the specified component from the registry and
+		// returns whether the operation was successful.
+		template <typename T>
+		bool RemoveComponent()
+		{
+			// Prevent removing the transform component
+			if (std::is_same_v<T, TransformComponent>)
+			{
+				return false;
+			}
+
+			auto component = GetComponent<T>();
+			if (component)
+			{
+				auto it = std::remove(m_ComponentRegistry.begin(), m_ComponentRegistry.end(), component);
+				bool found = it != m_ComponentRegistry.end();
+
+				if (found)
+				{
+					m_ComponentRegistry.erase(it, m_ComponentRegistry.end());
+					return true;
+				}
+			}
+
+			return false;
+		}
+
+		// Returns the first component that matches
+		// the specified type, otherwise nullptr.
+		template <typename T>
+		std::shared_ptr<T> GetComponent()
+		{
+			for (const auto& component : m_ComponentRegistry)
+			{
+				auto foundComponent = std::dynamic_pointer_cast<T>(component);
+				if (foundComponent)
+				{
+					return foundComponent;
+				}
+			}
+
+			return nullptr;
+		}
+
+		// Returns the transform component.
+		std::shared_ptr<TransformComponent> Transform()
+		{
+			return GetComponent<TransformComponent>();
+		}
+
 	private:
 		std::string m_Name;
 		std::string m_Tag;
 		std::shared_ptr<World> m_AttachedWorld;
+		std::vector<std::shared_ptr<Component>> m_ComponentRegistry;
+
+		[[nodiscard]] std::shared_ptr<Entity> GetPointer() const;
 	};
 }
