ecf6da5129c9a93f03ceebef43cddc528458b415
Futureblur <itsfutureblur@gmail.com>
2022-05-15 18:56:28 +0200

    Add MVP support to shader

--- Engine/Source/Shaders/UnlitVertexShader.glsl
+++ Engine/Source/Shaders/UnlitVertexShader.glsl
@@ -1,7 +1,12 @@
 #version 410 core
 layout (location = 0) in vec3 POS;
 
+uniform mat4 uModel;
+uniform mat4 uView;
+uniform mat4 uProjection;
+
 void main()
 {
-    gl_Position = vec4(POS.xyz, 1.0);
+    vec4 vertexPos = uProjection * uView * uModel * vec4(POS, 1.0);
+    gl_Position = vertexPos;
 }
