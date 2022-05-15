e1b7506fff6152e92fea2e48c8b4d18739b8637d
Futureblur <itsfutureblur@gmail.com>
2022-05-15 18:56:09 +0200

    Update `Renderer` class so it supports cameras

--- Engine/Source/Runtime/Renderer/Renderer.cpp
+++ Engine/Source/Runtime/Renderer/Renderer.cpp
@@ -11,12 +11,14 @@
 #include "World/Entity.h"
 #include "World/World.h"
 #include "World/WorldManager.h"
+#include "World/Components/CameraComponent.h"
 
 namespace Nightly
 {
 	std::unique_ptr<Shader> Renderer::m_VertexShader;
 	std::unique_ptr<Shader> Renderer::m_FragmentShader;
 	std::shared_ptr<ShaderProgram> Renderer::m_ShaderProgram;
+	std::unique_ptr<Entity> Renderer::m_FallbackCamera;
 
 	Renderer::~Renderer() = default;
 
@@ -34,11 +36,20 @@ namespace Nightly
 		m_ShaderProgram->Attach(m_FragmentShader.get());
 		m_ShaderProgram->Link();
 		m_ShaderProgram->Use();
+
+		m_FallbackCamera = std::make_unique<Entity>("Fallback Camera");
+		m_FallbackCamera->Initialize(nullptr);
+		m_FallbackCamera->AddComponent<CameraComponent>(std::make_shared<CameraComponent>(60, 0.1f, 1000.0f));
 	}
 
 	void Renderer::Update()
 	{
+		ClearColor();
+
 		m_ShaderProgram->Use();
+
+		m_ShaderProgram->SetUniformMatrix4fv("uView", m_FallbackCamera->GetComponent<CameraComponent>()->GetView());
+		m_ShaderProgram->SetUniformMatrix4fv("uProjection", m_FallbackCamera->GetComponent<CameraComponent>()->GetProjection());
 
 		// TODO: Move mesh components into separate registry to increase performance
 		for (const auto& entity : WorldManager::GetActiveWorld()->m_EntityRegistry)
@@ -49,5 +60,11 @@ namespace Nightly
 				mesh->Draw();
 			}
 		}
+	}
+
+	void Renderer::ClearColor()
+	{
+		glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+		glClearColor(27 / 255.f, 25 / 255.f, 50 / 255.f, 1);
 	}
 }
--- Engine/Source/Runtime/Renderer/Renderer.h
+++ Engine/Source/Runtime/Renderer/Renderer.h
@@ -4,6 +4,7 @@
 
 namespace Nightly
 {
+	class Entity;
 	class Shader;
 	struct ShaderProgram;
 
@@ -18,9 +19,19 @@ namespace Nightly
 
 		static void Update();
 
+		static std::shared_ptr<ShaderProgram> GetShaderProgram()
+		{
+			return m_ShaderProgram;
+		}
+
+	private:
+		static void ClearColor();
+
 	private:
 		static std::unique_ptr<Shader> m_VertexShader;
 		static std::unique_ptr<Shader> m_FragmentShader;
 		static std::shared_ptr<ShaderProgram> m_ShaderProgram;
+
+		static std::unique_ptr<Entity> m_FallbackCamera;
 	};
 }
