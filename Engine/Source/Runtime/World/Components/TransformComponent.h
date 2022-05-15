21fb2e5d2f87135273c5c5a732e69796794f4a26
Futureblur <itsfutureblur@gmail.com>
2022-05-15 15:27:52 +0200

    Update `TransformComponent.h`

--- Engine/Source/Runtime/World/Components/TransformComponent.h
+++ Engine/Source/Runtime/World/Components/TransformComponent.h
@@ -14,6 +14,18 @@ namespace Nightly
 
 		NL_DEFINE_COMPONENT(TransformComponent)
 
+		// Resets the position, rotation and scale to their default values.
+		void Reset()
+		{
+			m_Position = Vec3(0.0f);
+			m_Rotation = Vec3(0.0f);
+			m_Scale = Vec3(1.0f);
+		}
+
+		void Translate(const Vec3& position);
+		void Rotate(const Vec3& rotation);
+		void Scale(const Vec3& scale);
+
 		// Returns the transformation matrix.
 		[[nodiscard]] Mat4 GetTransform() const
 		{
@@ -27,6 +39,37 @@ namespace Nightly
 			return transform;
 		}
 
+		void SetPosition(const Vec3& position)
+		{
+			m_Position = position;
+		}
+
+		[[nodiscard]] Vec3 GetPosition() const
+		{
+			return m_Position;
+		}
+
+		void SetRotation(const Vec3& rotation)
+		{
+			m_Rotation = rotation;
+		}
+
+		[[nodiscard]] Vec3 GetRotation() const
+		{
+			return m_Rotation;
+		}
+
+		void SetScale(const Vec3 scale)
+		{
+			m_Scale = scale;
+		}
+
+		[[nodiscard]] Vec3 GetScale() const
+		{
+			return m_Scale;
+		}
+
+	private:
 		Vec3 m_Position = Vec3(0.0f);
 		Vec3 m_Rotation = Vec3(0.0f);
 		Vec3 m_Scale = Vec3(1.0f);
