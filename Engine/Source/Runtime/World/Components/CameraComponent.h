22e1963c83a247b3225bcafee4432aa2bf30df0c
Futureblur <itsfutureblur@gmail.com>
2022-05-21 13:32:36 +0200

    Update `CameraComponent.h`

--- Engine/Source/Runtime/World/Components/CameraComponent.h
+++ Engine/Source/Runtime/World/Components/CameraComponent.h
@@ -3,6 +3,7 @@
 #include "Component.h"
 #include "Core/EngineAPI.h"
 #include "Core/Math.h"
+#include "Core/Core.h"
 
 namespace Nightly
 {
@@ -12,19 +13,21 @@ namespace Nightly
 		Orthographic
 	};
 
+	// The 'eye' of the player. Only one camera can draw to the screen at a time.
 	class NL_API CameraComponent : public Component
 	{
 	public:
-		CameraComponent(float fov, float near, float far)
+		CameraComponent(float fov, float aspectRatio, float near, float far)
 				: m_Projection(glm::perspective(glm::radians(fov), 1.6f, near, far)),
-				  m_ProjectionType(ProjectionType::Perspective), m_Bounds(0.0f), m_FOV(fov > 0 ? fov : 60), m_Near(near), m_Far(far)
+				  m_ProjectionType(ProjectionType::Perspective), m_Bounds(0.0f), m_FOV(fov > 0 ? fov : 60),
+				  m_AspectRatio(aspectRatio), m_Near(near), m_Far(far)
 		{
 		}
 
 		CameraComponent(float left, float right, float bottom, float top, float near, float far)
 				: m_Projection(glm::ortho(left, right, bottom, top, near, far)),
 				  m_ProjectionType(ProjectionType::Orthographic), m_Bounds(left, right, bottom, top),
-				  m_FOV(), m_Near(near), m_Far(far)
+				  m_FOV(), m_AspectRatio(), m_Near(near), m_Far(far)
 		{
 		}
 
@@ -32,10 +35,13 @@ namespace Nightly
 
 		NL_DEFINE_COMPONENT(CameraComponent)
 
-		virtual void Update();
+		// Refreshes the projection matrix of the camera.
+		// Don't call this every frame since it gets called
+		// automatically once any setting changes.
+		virtual void UpdateProjection();
 
 		// Returns the field of view of the camera.
-		[[nodiscard]] float GetFOV() const
+		NL_NODISCARD float GetFOV() const
 		{
 			return m_FOV;
 		}
@@ -45,14 +51,21 @@ namespace Nightly
 		void SetFOV(float fov)
 		{
 			if (fov > 0) m_FOV = fov;
+			UpdateProjection();
 		}
 
-		[[nodiscard]] ProjectionType GetProjectionType() const
+		NL_NODISCARD ProjectionType GetProjectionType() const
 		{
 			return m_ProjectionType;
 		}
 
-		[[nodiscard]] float GetNearClippingPlanes() const
+		void SetProjectionType(const ProjectionType& type)
+		{
+			m_ProjectionType = type;
+			UpdateProjection();
+		}
+
+		NL_NODISCARD float GetNearClippingPlanes() const
 		{
 			return m_Near;
 		}
@@ -60,9 +73,10 @@ namespace Nightly
 		void SetNearClippingPlanes(float near)
 		{
 			if (near >= 0.0f && near < m_Far) m_Near = near;
+			UpdateProjection();
 		}
 
-		[[nodiscard]] float GetFarClippingPlanes() const
+		NL_NODISCARD float GetFarClippingPlanes() const
 		{
 			return m_Far;
 		}
@@ -70,16 +84,41 @@ namespace Nightly
 		void SetFarClippingPlanes(float far)
 		{
 			if (far > m_Near) m_Far = far;
+			UpdateProjection();
+		}
+
+		NL_NODISCARD float GetAspectRatio() const
+		{
+			return m_AspectRatio;
+		}
+
+		void SetAspectRatio(float ratio)
+		{
+			if (ratio > 0) m_AspectRatio = ratio;
+			UpdateProjection();
+		}
+
+		// Returns the viewport bounds if the projection type is set to orthographic.
+		NL_NODISCARD Vec4 GetBounds() const
+		{
+			return m_Bounds;
+		}
+
+		// Sets the viewport bounds if the projection type is set to orthographic.
+		void SetBounds(const Vec4& bounds)
+		{
+			m_Bounds = bounds;
+			UpdateProjection();
 		}
 
 		// Returns the view matrix of the camera.
-		[[nodiscard]] Mat4 GetView() const
+		NL_NODISCARD Mat4 GetView() const
 		{
 			return m_View;
 		}
 
 		// Returns the projection matrix of the camera.
-		[[nodiscard]] Mat4 GetProjection() const
+		NL_NODISCARD Mat4 GetProjection() const
 		{
 			return m_Projection;
 		}
@@ -90,6 +129,7 @@ namespace Nightly
 		float m_FOV;
 		float m_Near;
 		float m_Far;
+		float m_AspectRatio;
 
 		Vec4 m_Bounds;
 
