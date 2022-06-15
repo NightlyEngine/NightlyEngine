1952b5bf0c1f5bc03225b5615eab5140bfbd9eb3
Futureblur <itsfutureblur@gmail.com>
2022-06-15 09:53:50 +0200

    Update Window class to work with Vulkan

--- Engine/Source/Runtime/Core/Window.cpp
+++ Engine/Source/Runtime/Core/Window.cpp
@@ -21,21 +21,27 @@ namespace Nightly
 		NL_ASSERT(WindowManager::IsInitialized(), "Initialize WindowManager first before creating the first window!", ENGINE);
 
 		// Set window hints
-		glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 4);
-		glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 1);
-		glfwWindowHint(GLFW_CLIENT_API, GLFW_OPENGL_API);
-		glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
-		glfwWindowHint(GLFW_OPENGL_FORWARD_COMPAT, GL_TRUE);
+		//glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 4);
+		//glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 1);
+
+		// TODO: Integrate properly with pipeline
+		glfwWindowHint(GLFW_CLIENT_API, GLFW_NO_API);
+		//glfwWindowHint(GLFW_CLIENT_API, GLFW_OPENGL_API);
+
+		//glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+		//glfwWindowHint(GLFW_OPENGL_FORWARD_COMPAT, GL_TRUE);
+
+		glfwWindowHint(GLFW_RESIZABLE, GLFW_FALSE);
 
 		// Create window
 		m_Window = glfwCreateWindow(props.width, props.height, props.title.c_str(), nullptr, nullptr);
 		NL_ASSERT(m_Window, "Failed to create window!", ENGINE);
 		NL_CORE_INFO("Created new window: " << props.title, ENGINE);
 
-		MakeContextCurrent();
+		//MakeContextCurrent();
 
 		// Initialize the OpenGL API with GLAD
-		NL_ASSERT(gladLoadGLLoader((GLADloadproc) glfwGetProcAddress), "Failed to initialize OpenGL loader!", ENGINE);
+		//NL_ASSERT(gladLoadGLLoader((GLADloadproc) glfwGetProcAddress), "Failed to initialize OpenGL loader!", ENGINE);
 
 		m_Width = props.width;
 		m_Height = props.height;
@@ -67,7 +73,7 @@ namespace Nightly
 	void Window::Destroy()
 	{
 		if (!m_IsRunning) return;
-		
+
 		m_IsRunning = false;
 		glfwDestroyWindow(m_Window);
 	}
