6e3577e87a732b0cc200bf3563b5d97c36006090
Futureblur <itsfutureblur@gmail.com>
2022-06-15 09:47:13 +0200

    Update Core.h

--- Engine/Source/Runtime/Core/Core.h
+++ Engine/Source/Runtime/Core/Core.h
@@ -5,15 +5,18 @@
 
 #include "Log.h"
 
-#define NL_ASSERT(condition, message, source) NL_CORE_FATAL(message, source); return
+// Creates a string stream for concatenating multiple data types.
+#define NL_TEXT(string) std::stringstream("").str() << string
+
+#define NL_ASSERT(condition, message, source) NL_CORE_FATAL(message, source)
 #else
 
 #include <cassert>
 
-#define NL_ASSERT(condition, message, source) assert(condition && message)
-#endif
-
 // Creates a string stream for concatenating multiple data types.
 #define NL_TEXT(string) std::stringstream("") << string
+
+#define NL_ASSERT(condition, message, source) assert(condition && message)
+#endif
 
 #define NL_NODISCARD [[nodiscard]]
