4490134a81c2b94762b328fa91d5a6fec02092dd
Futureblur <itsfutureblur@gmail.com>
2022-06-17 20:57:33 +0200

    Create functions for concatenating multiple data types into one string

--- Engine/Source/Runtime/Core/Utils.h
+++ Engine/Source/Runtime/Core/Utils.h
@@ -2,6 +2,9 @@
 
 #include "EngineAPI.h"
 
+// Concatenates multiple data types into one string.
+#define NL_TEXT(args, ...) Utils::ToText(args, ##__VA_ARGS__)
+
 namespace NL
 {
 	class NL_API Utils
@@ -9,5 +12,19 @@ namespace NL
 	public:
 		static std::string ToUppercase(std::string text);
 		static std::string ToLowercase(std::string text);
+
+		template <typename T>
+		static std::string ToText(const T& text)
+		{
+			std::stringstream stream;
+			stream << text;
+			return stream.str();
+		}
+
+		template <typename T, typename ... Args>
+		static std::string ToText(const T& text, const Args& ... args)
+		{
+			return ToText(text) + ToText(args...);
+		}
 	};
 }
