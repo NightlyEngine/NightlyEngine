5491e39db9cb18ddff1b0fc46177b7116c904a11
Futureblur <itsfutureblur@gmail.com>
2022-06-17 16:21:32 +0200

    Fix circular dependency and `NL_TEXT` error

--- Engine/Source/Runtime/Core/Core.h
+++ Engine/Source/Runtime/Core/Core.h
@@ -1,30 +1,27 @@
 #pragma once
 
-// Assert
-#ifdef NDEBUG
+#define NL_NODISCARD [[nodiscard]]
 
-#include "Log.h"
+template <typename T>
+using Ref = std::shared_ptr<T>;
 
 // Creates a string stream for concatenating multiple data types.
-#define NL_TEXT(string) std::stringstream("").str() << string
+#define NL_TEXT(string) std::stringstream("") << string
+
+// Assert
+#ifndef NDEBUG
+
+#include "Log.h"
 
 #define NL_ASSERT(condition, message, source)           \
 do                                                      \
 {                                                       \
-	if(!(condition)) NL_CORE_FATAL(message, source);    \
+    if(!(condition)) NL_CORE_FATAL(message, source);    \
 }                                                       \
 while(0)
 #else
 
 #include <cassert>
 
-// Creates a string stream for concatenating multiple data types.
-#define NL_TEXT(string) std::stringstream("") << string
-
 #define NL_ASSERT(condition, message, source) assert(condition && message)
 #endif
-
-#define NL_NODISCARD [[nodiscard]]
-
-template <typename T>
-using Ref = std::shared_ptr<T>;
