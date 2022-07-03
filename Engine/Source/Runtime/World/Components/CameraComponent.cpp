bfad12b791f531f02a5b96b8eefa04f57e026b82
Futureblur <itsfutureblur@gmail.com>
2022-07-03 19:17:44 +0200

    Create OnDraw function for drawing the active world

--- Engine/Source/Runtime/World/Components/CameraComponent.cpp
+++ Engine/Source/Runtime/World/Components/CameraComponent.cpp
@@ -1,16 +1,70 @@
 #include "CameraComponent.h"
 
+#include "MeshComponent.h"
+
+#include "Core/Window.h"
+#include "Core/WindowManager.h"
+
+#include "Renderer/Shader.h"
+#include "Renderer/Renderer.h"
+#include "Renderer/RenderCommand.h"
+
+#include "World/World.h"
+#include "World/WorldManager.h"
+#include "World/Entity.h"
+
 namespace NL
 {
-	void CameraComponent::UpdateProjection()
+	CameraComponent::CameraComponent(const CameraSpecification& specification)
+			: Camera(specification)
 	{
-		if (m_ProjectionType == ProjectionType::Perspective)
+		// Create framebuffer
+		int width = WindowManager::GetCurrentWindow()->GetWidth();
+		int height = WindowManager::GetCurrentWindow()->GetHeight();
+
+		FramebufferProps props(width, height, false);
+		m_Framebuffer = Framebuffer(props);
+
+		if (!m_ScreenTarget)
 		{
-			m_Projection = glm::perspective(glm::radians(m_FOV), m_AspectRatio, m_Near, m_Far);
+			Framebuffer::Unbind();
 		}
-		else
+	}
+
+	void CameraComponent::OnDraw(const ShaderProgram& shader)
+	{
+		RenderCommand::ClearColor(ClearColor());
+		RenderCommand::Clear(OpenGLBufferBit::COLOR | OpenGLBufferBit::DEPTH);
+
+		if (m_ScreenTarget)
 		{
-			m_Projection = glm::ortho(m_Bounds.x, m_Bounds.y, m_Bounds.z, m_Bounds.w, m_Near, m_Far);
+			m_Framebuffer.Bind();
+		}
+
+		RenderCommand::EnableFeature(OpenGLFeature::DEPTH_TEST);
+		RenderCommand::ClearColor(ClearColor());
+		RenderCommand::Clear(OpenGLBufferBit::COLOR | OpenGLBufferBit::DEPTH);
+
+		shader.Use();
+		shader.SetUniformMat4("u_ProjectionView", GetProjectionViewMatrix());
+
+		// TODO: Move mesh components into separate registry to increase performance
+		for (const auto& entity : WorldManager::GetActiveWorld()->GetEntityRegistry())
+		{
+			auto mesh = entity->GetComponent<MeshComponent>();
+			if (mesh)
+			{
+				mesh->Draw();
+			}
+		}
+
+		RenderCommand::DisableFeature(OpenGLFeature::DEPTH_TEST);
+
+		if (m_ScreenTarget)
+		{
+			Framebuffer::Unbind();
+			RenderCommand::ClearColor(ClearColor());
+			RenderCommand::Clear(OpenGLBufferBit::COLOR | OpenGLBufferBit::DEPTH);
 		}
 	}
 }
