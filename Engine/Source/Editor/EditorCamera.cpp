b5a553f8983ae12ed13e0d725c430af7eb6e7be0
Futureblur <itsfutureblur@gmail.com>
2022-07-03 20:59:28 +0200

    Create EditorCamera class for navigating in the editor

---
+++ Engine/Source/Editor/EditorCamera.cpp
@@ -0,0 +1,103 @@
+#include "EditorCamera.h"
+
+#include <glm/glm.hpp>
+
+#include "Core/Event/InputSystem.h"
+
+#include "Renderer/RenderCommand.h"
+#include "Renderer/Shader.h"
+
+#include "World/World.h"
+#include "World/WorldManager.h"
+#include "World/Entity.h"
+#include "World/Components/MeshComponent.h"
+
+#include "Core/Window.h"
+#include "Core/WindowManager.h"
+
+namespace NLE
+{
+	EditorCamera::EditorCamera(float fov, float aspectRatio, float near, float far)
+			: Camera(CameraSpecification { fov, aspectRatio, near, far }), m_TargetPosition(m_Position)
+	{
+		// Create framebuffer
+		int width = WindowManager::GetCurrentWindow()->GetWidth();
+		int height = WindowManager::GetCurrentWindow()->GetHeight();
+
+		FramebufferProps props(width, height, false);
+		m_Framebuffer = Framebuffer(props);
+	}
+
+	void EditorCamera::OnDraw(const ShaderProgram& shader)
+	{
+		RenderCommand::ClearColor(ClearColor());
+		RenderCommand::Clear(OpenGLBufferBit::COLOR | OpenGLBufferBit::DEPTH);
+
+		m_Framebuffer.Bind();
+
+		RenderCommand::EnableFeature(OpenGLFeature::DEPTH_TEST);
+		RenderCommand::ClearColor(ClearColor());
+		RenderCommand::Clear(OpenGLBufferBit::COLOR | OpenGLBufferBit::DEPTH);
+
+		shader.Use();
+		shader.SetUniformMat4("u_ProjectionView", GetProjectionViewMatrix());
+
+		// TODO: Move mesh components into separate registry to increase performance
+		for (const auto& entity : WorldManager::GetActiveWorld()->GetEntityRegistry())
+		{
+			auto mesh = entity->GetComponent<MeshComponent>();
+			if (mesh)
+			{
+				mesh->Draw();
+			}
+		}
+
+		RenderCommand::DisableFeature(OpenGLFeature::DEPTH_TEST);
+
+		Framebuffer::Unbind();
+		RenderCommand::ClearColor(ClearColor());
+		RenderCommand::Clear(OpenGLBufferBit::COLOR | OpenGLBufferBit::DEPTH);
+	}
+
+	void EditorCamera::Update(float deltaTime)
+	{
+		Vec3 direction(0.0f);
+
+		if (InputSystem::IsKeyPressed(NL_KEY_W))
+		{
+			direction += m_Front;
+		}
+
+		if (InputSystem::IsKeyPressed(NL_KEY_S))
+		{
+			direction -= m_Front;
+		}
+
+		if (InputSystem::IsKeyPressed(NL_KEY_A))
+		{
+			direction -= glm::cross(m_Front, m_Up);
+		}
+
+		if (InputSystem::IsKeyPressed(NL_KEY_D))
+		{
+			direction += glm::cross(m_Front, m_Up);
+		}
+
+		if (InputSystem::IsKeyPressed(NL_KEY_Q))
+		{
+			direction -= m_Up;
+		}
+
+		if (InputSystem::IsKeyPressed(NL_KEY_E))
+		{
+			direction += m_Up;
+		}
+
+		if (direction != Vec3(0.0f))
+		{
+			m_TargetPosition += glm::normalize(direction) * m_MovementSpeed * 0.01f;
+		}
+
+		m_View = glm::lookAt(m_Position, m_Position + m_Front, m_Up);
+	}
+}
---
+++ Engine/Source/Editor/EditorCamera.h
@@ -0,0 +1,35 @@
+#pragma once
+
+#include "Core/Core.h"
+#include "Core/Math.h"
+#include "Renderer/Camera.h"
+
+using namespace NL;
+
+namespace NLE
+{
+	class EditorCamera : public Camera
+	{
+	public:
+		explicit EditorCamera(float fov = 70.0f, float aspectRatio = 16.0f / 9.0f, float near = 0.1f, float far = 500.0f);
+
+		~EditorCamera() override = default;
+
+		void OnDraw(const ShaderProgram& shader) override;
+
+		void Update(float deltaTime);
+
+	private:
+		Vec3 m_Position = Vec3(0.0f);
+		Vec3 m_TargetPosition;
+
+		float m_MovementSpeed = 3.0f;
+		float m_SpeedMultiplier = 2.0f;
+
+		Vec3 m_Up = Vec3(0.0f, 1.0f, 0.0f);
+		Vec3 m_Front = Vec3(0.0f, 0.0f, -1.0f);
+
+		float m_Yaw = 0.0f;
+		float m_Pitch = 0.0f;
+	};
+}
