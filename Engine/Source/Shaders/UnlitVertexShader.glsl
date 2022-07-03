093db0313d66ac0c525cb0dffff7dce545522cee
Futureblur <itsfutureblur@gmail.com>
2022-07-03 19:15:40 +0200

    Use ProjectionView matrix for hopefully better performance - This way, the matrices will be calculated into the CPU before being sent to the fragment shader

--- Engine/Source/Shaders/UnlitVertexShader.glsl
+++ Engine/Source/Shaders/UnlitVertexShader.glsl
@@ -1,12 +1,11 @@
 #version 410 core
 layout (location = 0) in vec3 POS;
 
-uniform mat4 uModel;
-uniform mat4 uView;
-uniform mat4 uProjection;
+uniform mat4 u_Model;
+uniform mat4 u_ProjectionView;
 
 void main()
 {
-    vec4 vertexPos = uProjection * uView * uModel * vec4(POS, 1.0);
+    vec4 vertexPos = u_ProjectionView * u_Model * vec4(POS, 1.0);
     gl_Position = vertexPos;
 }
