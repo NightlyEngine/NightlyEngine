3aed58574f35333126d09c97e4538e85347ed552
Futureblur <itsfutureblur@gmail.com>
2022-07-05 19:21:10 +0200

    Create premake5.lua

---
+++ premake5.lua
@@ -0,0 +1,31 @@
+ThirdPartyPath = "%{wks.location}/Engine/Source/ThirdParty"
+RuntimePath = "%{wks.location}/Engine/Source/Runtime"
+BuildPath = "%{wks.location}/Engine/Build/%{wks.buildcfg}"
+
+ThirdParty = {}
+ThirdParty["glad"] = 	(ThirdPartyPath .. "/glad")
+ThirdParty["glfw"] = 	(ThirdPartyPath .. "/glfw")
+ThirdParty["glm"] = 	(ThirdPartyPath .. "/glm")
+ThirdParty["imgui"] = 	(ThirdPartyPath .. "/imgui-cmake")
+
+workspace "NightlyEngine"
+	architecture "x64"
+	cppdialect "C++20"
+
+	targetdir "Engine/Binaries/%{cfg.buildcfg}"
+
+	configurations
+	{
+		"Debug",
+		"Release"
+	}
+
+group "ThirdParty"
+	include "Engine/Source/ThirdParty/glad"
+	include "Engine/Source/ThirdParty/glfw"
+	include "Engine/Source/ThirdParty/imgui-cmake"
+group ""
+
+include "Engine/Source/Runtime"
+include "Engine/Source/Editor"
+include "Engine/Source/Plugins"
