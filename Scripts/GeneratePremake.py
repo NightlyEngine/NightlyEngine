94c32d3b627c0d7c040662234c56c1c5ea78e50b
Futureblur <itsfutureblur@gmail.com>
2022-07-05 19:21:20 +0200

    Create premake generator script

---
+++ Scripts/GeneratePremake.py
@@ -0,0 +1,16 @@
+import os
+from Utils import log_info, log_fail, get_os
+
+os.chdir("..")
+
+generator = input("Select generator (vs2019/vs2022/xcode4/gmake2): ")
+
+premake_cmd = f"Engine/Source/ThirdParty/premake/premake5 {generator}"
+
+log_info("Generating premake files...")
+
+if os.system(premake_cmd) != 0:
+	log_fail("Failed to generate premake files!")
+	exit()
+	
+log_info("Successfully generated project files!")
