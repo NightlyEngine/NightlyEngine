006e4efcfe8dc0432ee23ca438a69628f638e0cd
Futureblur <itsfutureblur@gmail.com>
2022-07-07 14:46:27 +0200

    Replace premake path on Windows

--- Scripts/GeneratePremake.py
+++ Scripts/GeneratePremake.py
@@ -7,6 +7,10 @@ generator = input("Select generator (vs2019/vs2022/xcode4/gmake2): ")
 
 premake_cmd = f"Engine/Source/ThirdParty/premake/premake5 {generator}"
 
+# Replace premake path on Windows
+if get_os() == "Windows":
+    premake_cmd = premake_cmd.replace("/", "\\")
+
 log_info("Generating premake files...")
 
 if os.system(premake_cmd) != 0:
